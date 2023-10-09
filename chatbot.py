f679d7e50610c8dec2f305121a88db78f5899472
Shawn Gestupa <thereal.nwahs@gmail.com>
2023-10-09 15:45:25 +0800

    pls work :(

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@
 """# Import Necessary Libraries
 
 import time
+import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
